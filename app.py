5290399cfa9ad3f63ad5da14841a6b91dbf782fb
TsaiRongFu <q0975040879@gmail.com>
2021-05-19 21:14:11 +0800

    update2

--- app.py
+++ app.py
@@ -409,7 +409,6 @@ def DataInsertToFlexSendMessage(DataList):
     )
     return flex_message
 
-
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 300))
     app.run(host='0.0.0.0', port=port)
