f11b6d88775366f36d0744b75dd36b4409ab5851
TsaiRongFu <q0975040879@gmail.com>
2021-05-26 23:41:06 +0800

    Update

--- https.py
+++
@@ -1,7 +0,0 @@
-import requests
-userid = ""
-URL = "https://api.line.me/v2/bot/profile/"+ userid
-header = {'Authorization': 'Bearer ..........................'}
-r = requests.get(URL, headers = header)
-
-print(r.json()['userId'])
