8e1d753b4a86dcaa6f8e280f3f973c2de40e0546
TsaiRongFu <q0975040879@gmail.com>
2021-06-06 19:18:03 +0800

    update

--- app.py
+++ app.py
@@ -29,27 +29,26 @@ LineBotApiKey = (config['LineToken']['LineBotApiKey'])
 WebhookHandlerKey = (config['LineToken']['WebhookHandler'])
 settings_path = './key/settings.yaml'
 # gauth = GoogleAuth(settings_file=settings_path)
-# # gauth.LocalWebserverAuth()
+# gauth.LocalWebserverAuth()
 # drive = GoogleDrive(gauth)
 
-# Channel Access Token
 line_bot_api = LineBotApi(LineBotApiKey)
-# Channel Secret
+
 handler = WebhookHandler(WebhookHandlerKey)
 
 @app.route('/')
 def index():
     return "<p>Line Bot Connection To Ngrok Success!</p>"
 
-# 監聽所有來自 /callback 的 Post Request
+
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
+
     signature = request.headers['X-Line-Signature']
-    # get request body as text
+
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    # handle webhook body
+
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
